99fd400d39444e87540aa1203ba9ba454f855b55
khan <khan.ahmed@city.ac.uk>
2024-04-06 22:35:04 +0100

    update

---
+++ components/MainContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- 
+      - Get posts from db
+      - Render posts
+     -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,58 @@
-<template lang="pug">
-.nav
-  li
-    a foo
-  li
-    a bar
+<template>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li  class="nav-item" v-for="link in navbar_links">
+          <a class="nav-link" href="{{ link.link }}">{{ link.text }}</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li> -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script lang="ts" setup>
+let navbar_links = [
+  {text: "foo1", link: "#"},
+  {text: "foo2", link: "#"},
+  {text: "foo3", link: "#"},
+  {text: "foo4", link: "#"},
+  {text: "foo5", link: "#"},
+  {text: "foo6", link: "#"},
+  {text: "foo7", link: "#"},
+  {text: "foo8", link: "#"},
+  {text: "foo9", link: "#"},
+  {text: "foo10", link: "#"},
+  {text: "foo11", link: "#"},
+]
 </script>
 
 <style>
